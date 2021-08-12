63df3ab1fce27a4ad2ef0f3a96b23e332ec4c219
Kalin Karaliev <50074330+karalkal@users.noreply.github.com>
2021-08-12 20:27:19 +0100

    Delete 04_Functions/1_lab directory

--- 04_Functions/1_lab/lab3_count_a_string.py
+++
@@ -1,9 +0,0 @@
-def count_a_string(string, times):
-    res = string * times
-    return res
-
-
-string = input()
-times = int(input())
-
-print(count_a_string(string, times))
--- 04_Functions/1_lab/lab5_rectangle_area.py
+++
@@ -1,8 +0,0 @@
-def area_rect(a, b):
-    return a * b
-
-
-a = int(input())
-b = int(input())
-
-print(area_rect(a, b))
